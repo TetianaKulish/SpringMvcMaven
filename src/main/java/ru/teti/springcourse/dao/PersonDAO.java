9b6722f1a7f2b48e3333e8435d2939b3988ce52b
tkuli <teti98kulish@gmail.com>
2022-04-18 23:53:02 +0200

    create person app without DB use

---
+++ src/main/java/ru/teti/springcourse/dao/PersonDAO.java
@@ -0,0 +1,46 @@
+package ru.teti.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.teti.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findFirst().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
+}
